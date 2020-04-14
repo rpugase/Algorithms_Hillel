9e5676f769ea5fc03778f18a9553785ce388aa7a
rpugase <rpugase@gmail.com>
2020-04-14 20:48:37 +0300

    finish sort time

--- third/third_test.cpp
+++ third/third_test.cpp
@@ -56,7 +56,9 @@ void test_sort_time() {
     };
 
     sort(times.begin(), times.end(), [] (vector<int>& a, vector<int>& b) {
-        return a[0] < b[0];
+        return a[0] < b[0] ||
+                (a[0] == b[0] && a[1] < b[1]) ||
+                (a[0] == b[0] && a[1] == b[1] && a[2] < b[2]);
     });
 
     assert(times == result);
